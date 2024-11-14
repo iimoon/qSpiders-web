dbb7e33b6cdad9ac0e86a97bcd5ca6844004761e
iimoon <aymanabdk@gmail.com>
2024-11-14 22:29:11 +0530

    Added lottie and applied some css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import qspiderLogo from "../assets/logo/qspidersvg.svg";
 import "../styles/home.css";
 import menuicon from "../assets/icons/menu.svg";
 import closeicon from "../assets/icons/close.svg";
+import codingAnimation from '../assets/lottie-assets/coding.json'
+import Lottie from "lottie-react";
 
 const Home = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
-      <nav className="nav-container">
-        <h3 id="logo">Qspiders</h3>
-        <ul className="nav-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Courses</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#" id="bookdemo">BOOK A DEMO</a></li>
-          <li className="menu-btn">
-            <img src={menuicon} alt="Menu Icon" height={26} width={26} />
-          </li>
-        </ul>
-      </nav>
+      <div className="header-color">
+        <nav className="nav-container">
+          <h3 id="logo">Qspiders</h3>
+          <ul className={`sidebar ${isSidebarOpen ? "active" : ""}`}>
+            <li onClick={toggleSidebar}>
+              <img src={closeicon} id="icon" alt="Close Icon" />
+            </li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#" id="bookdemo-res">BOOK A DEMO</a></li>
+          </ul>
+          <ul className="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#" id="bookdemo">BOOK A DEMO</a></li>
+          </ul>
+          {/* Menu button */}
+          <div className="menu-btn" onClick={toggleSidebar}>
+            <img src={menuicon} id="icon" alt="Menu Icon" height={26} width={26} />
+          </div>
+        </nav>
+
+        <div className="intro-container">
+          <div className="intro-text">
+            <h1 id="main-text">Level up your development skills in Qspiders</h1>
+            <h2 id="sub-text">Learn from experts who have real industry experience and get certified!</h2>
+          </div>
+          <div className="intro-banner">
+            <Lottie animationData={codingAnimation} loop={true}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
